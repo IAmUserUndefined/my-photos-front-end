ba9f9dddb99b651509cf6c7d6fc985b9bfa97536
João Pedro <jpmns9103@gmail.com>
2021-12-12 23:01:26 -0300

    criar função de registro de usuário

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 import Form from '../../styles/form';
@@ -8,13 +8,80 @@ import Button from '../../components/Button';
 import LinkForm from '../../components/LinkForm';
 import Header from '../../components/Header';
 
+import api from "../../services/api";
+
+import isEmailValid from "../../utils/isEmailValid";
+import isPasswordValid from "../../utils/isPasswordValid";
+import LoadingGif from "../../components/LoadingGif/index";
+
+import { useModal } from "../../providers/ModalProvider";
+
 const Register = () => {
 
+    const { handleShowModal } = useModal();
+    const [buttonChildren, setButtonChildren] = useState("Cadastrar");
     const navigate = useNavigate();
 
     const handleLink = (link) => {
         navigate(link);
     };
+
+    const handleRegister = async () => {
+        setButtonChildren(<LoadingGif />);
+    
+        const form = document.forms.register;
+    
+        let { email, password, passwordConfirm } = form;
+    
+        if (!email.value || !password.value || !passwordConfirm.value) {
+          setButtonChildren("Cadastrar");
+          return handleShowModal("Preencha todos os campos");
+        }
+    
+        if (!isEmailValid(email.value)) {
+          setButtonChildren("Cadastrar");
+          email.value = "";
+          return handleShowModal("Coloque um email válido");
+        }
+    
+        const { result, message } = isPasswordValid(password.value);
+    
+        if (!result) {
+          setButtonChildren("Cadastrar");
+          return handleShowModal(message);
+        }
+    
+        if (password.value !== passwordConfirm.value) {
+          setButtonChildren("Cadastrar");
+          password.value = "";
+          passwordConfirm.value = "";
+          return handleShowModal("As senhas não coincidem");
+        }
+    
+        await api
+          .post("/user/create", {
+            email: email.value,
+            password: password.value,
+            passwordConfirm: passwordConfirm.value,
+          })
+          .then(({ data }) => {
+            email.value = "";
+            password.value = "";
+            passwordConfirm.value = "";
+            handleShowModal(data.response);
+          })
+          .catch(({ response }) =>
+            response
+              ? handleShowModal(response.data.response)
+              : handleShowModal("Erro no Servidor")
+          );
+    
+        email.value = "";
+        password.value = "";
+        passwordConfirm.value = "";
+    
+        setButtonChildren("Cadastrar");
+      };
   
     return ( 
         <>
@@ -24,7 +91,7 @@ const Register = () => {
                 <InputForm type="email" name="email" placeholder="Email"/>
                 <InputForm type="password" name="password" placeholder="Senha"/>
                 <InputForm type="password" name="passwordConfirm" placeholder="Confirmação de Senha"/>
-                <Button>Cadastrar</Button>
+                <Button onClick={() => handleRegister()}>{buttonChildren}</Button>
                 <LinkForm onClick={() => handleLink("/")}>Já tem um cadastro?</LinkForm>
             </Form>
         </>
