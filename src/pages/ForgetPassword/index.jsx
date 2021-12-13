fa06e14e87b62986f300ba95687d4a5aaa7a4ada
João Pedro <jpmns9103@gmail.com>
2021-12-12 23:40:03 -0300

    criar função de envio de email de recuperação de senha

--- src/pages/ForgetPassword/index.jsx
+++ src/pages/ForgetPassword/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 import Form from '../../styles/form';
@@ -7,6 +7,13 @@ import InputForm from "../../components/InputForm/index"
 import Button from '../../components/Button';
 import LinkForm from '../../components/LinkForm';
 import Header from '../../components/Header';
+import LoadingGif from '../../components/LoadingGif/index';
+
+import api from "../../services/api";
+
+import isEmailValid from "../../utils/isEmailValid";
+
+import { useModal } from "../../providers/ModalProvider";
 
 const ForgetPassword = () => {
 
@@ -15,6 +22,45 @@ const ForgetPassword = () => {
     const handleLink = (link) => {
         navigate(link);
     };
+
+    const [buttonChildren, setButtonChildren] = useState("Enviar Email");
+    const { handleShowModal } = useModal();
+  
+    const handleForgetPassword = async () => {
+      setButtonChildren(<LoadingGif />);
+  
+      const form = document.forms.forgetPassword;
+  
+      let { email } = form;
+  
+      if (!email.value) {
+        setButtonChildren("Enviar Email");
+        return handleShowModal("Preencha o campo de email");
+      }
+  
+      if (!isEmailValid(email.value)) {
+        setButtonChildren("Enviar Email");
+        email.value = "";
+        return handleShowModal("Coloque um email válido");
+      }
+  
+      await api
+        .post("/user/password/send-token-password-recover", {
+          email: email.value,
+        })
+        .then(({ data }) => {
+          handleShowModal(data.response);
+        })
+        .catch(({ response }) =>
+          response
+            ? handleShowModal(response.data.response)
+            : handleShowModal("Erro no Servidor")
+        );
+  
+      email.value = "";
+  
+      setButtonChildren("Enviar Email");
+    };
   
     return ( 
         <>
@@ -22,7 +68,7 @@ const ForgetPassword = () => {
 
             <Form name="forgetPassword">
                 <InputForm type="email" name="email" placeholder="Email"/>
-                <Button>Enviar Email de Recuperação</Button>
+                <Button onClick={() => handleForgetPassword()}>{buttonChildren}</Button>
                 <LinkForm onClick={() => handleLink("/")}>Ainda não tem um cadastro?</LinkForm>
             </Form>
         </>
