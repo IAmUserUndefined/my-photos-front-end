b12d03d155cf15f7a9fee5a2754586c80f245c07
João Pedro <jpmns9103@gmail.com>
2022-01-16 23:15:28 -0300

    atualizar função isEmailValid

--- src/utils/isEmailValid.js
+++ src/utils/isEmailValid.js
@@ -1,11 +1,5 @@
 const isEmailValid = (email) => {
-    const [, domainAndDotCom] = email.split("@");
-    const [domain] = domainAndDotCom.split(".");
-
-    if(domain.length > 20)
-        return false;
-    
-    return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/.test(email);
+    return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
 
 export default isEmailValid;
